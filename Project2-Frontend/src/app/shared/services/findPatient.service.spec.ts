e5b912d70819a7c42f7d680b82a784f434780ab3
Asher Williams <asher.williams1998@gmail.com>
2020-07-29 04:01:40 -0500

    finished testing for findPatient service

--- Project2-Frontend/src/app/shared/services/findPatient.service.spec.ts
+++ Project2-Frontend/src/app/shared/services/findPatient.service.spec.ts
@@ -7,7 +7,6 @@ import { Patient } from '../models/patient';
 describe('FindPatientService', () => {
   let service: FindPatientService;
   let httpMock: HttpTestingController;
-  let serviceUrl: 'https://localhost:44362/api/Patients';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -36,8 +35,8 @@ describe('FindPatientService', () => {
       {
         patientId: 1,
         patientRoomId: 5,
-        firstName: 'Test1',
-        lastName: 'TestLast1',
+        firstName: 'Test',
+        lastName: 'TestLast',
       };
 
     service.getPatientByRoom(5).subscribe(patient => {
@@ -48,5 +47,56 @@ describe('FindPatientService', () => {
     expect(request.request.method).toBe('GET');
 
     request.flush(patientItem);
+  })
+
+  it('should fetch patients as observable', () => {
+    const tableItems: any[] = [
+      // {
+      //   doctorId: 2,
+      //   firstName: "Test",
+      //   lastName: "TestLast"
+      // },
+      // {
+      //   treatmentId: 4,
+      //   illnessId: 1,
+      //   doctorId: 2,
+      //   name: "TestTreatment",
+      //   timeToTreat: "3"
+      // },
+      // {
+      //   treatmentDetailsId: 1,
+      //   treatmentId: 4,
+      //   patientId: 1,
+      //   opsRoomId: 1,
+      //   startTime: ""
+      // },
+      // {
+      //   treatmentDetailsId: 2,
+      //   treatmentId: 4,
+      //   patientId: 2,
+      //   opsRoomId: 2,
+      //   startTime: ""
+      // },
+      {
+        patientId: 1,
+        patientRoomId: 1,
+        firstName: "PTest1",
+        lastName: "PTestLast1"
+      },
+      {
+        patientId: 2,
+        patientRoomId: 2,
+        firstName: "PTest2",
+        lastName: "PTestLast2"
+      }
+    ]
+
+    service.getDocterPatients(2).subscribe( patients => {
+      expect(patients.length).toBe(2);
+    });
+    const request = httpMock.expectOne(`https://localhost:44362/api/Patients/Doctors/2`);
+    expect(request.request.method).toBe('GET');
+
+    request.flush(tableItems);
   })
 });
