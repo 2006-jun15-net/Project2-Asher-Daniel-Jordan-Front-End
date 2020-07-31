ac8e93ec76fd0ac840b53d3b17dda2cf0cb02e89
Daniel Aasa <danielaasa42@gmail.com>
2020-07-30 22:58:41 -0700

    fixed testing

--- Project2-Frontend/src/app/register/register.component.spec.ts
+++ Project2-Frontend/src/app/register/register.component.spec.ts
@@ -1,14 +1,11 @@
-import 'jasmine';
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
-<<<<<<< HEAD
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-=======
+
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
->>>>>>> 5857277c7424987060f0a331a6f5b0944fd0a3b1
+
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -17,14 +14,9 @@ describe('RegisterComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ RegisterComponent ],
-<<<<<<< HEAD
-      imports: [ 
-        HttpClientTestingModule,
-        RouterTestingModule,
-      ]
-=======
+
+
       imports: [RouterTestingModule, HttpClientTestingModule]
->>>>>>> 5857277c7424987060f0a331a6f5b0944fd0a3b1
     })
     .compileComponents();
   }));
